9fa9b6e34a2d6e1ad5cf1e8b02315b16c0dc4663
guylamdan <guylamdan@mail.tau.ac.il>
2022-08-02 12:33:37 +0300

    0208 functions

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Guy &amp; Yair final software project 
 #change yair
 #change Guy 
+
+#Main Functions: 
+1) form the W matrix from the input
+2) form the D matrix from W matrix
+3) form the L(norm) Matrix
+4) find the K eigenValues and eigenVectors
+5) form U from the K eigenVectors as colums
+6) form T from U
+7) call the K-means algo with T rows as vectors and k
+
---
+++ spkmeans.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+//#include "spkmeans.h"
+//#include "kmeans.c"
+
+double** formInputToMatrix(){ //Guy 
+
+}
+
+double** formWeightedMatrix(double** vectorsMatrix, int n)// Yair
+{
+}
+
+double** formDegreeMatrix (double** weightedMatrix, int n){ // Yair
+
+}
+
+double** formLnormMatrix (double** weightedMatrix, int n){ // Guy
+
+}
+
+int main(int argc, char *argv[]){ // Guy
+
+}
+
+
+
+
